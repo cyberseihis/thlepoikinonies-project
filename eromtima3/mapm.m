071e263cb71d918dc7976706e037baa14e3868d7
cyberseihis <mcraftdm@gmail.com>
2022-01-08 22:56:22 +0200

    mapm on erothma3 works when no noise is added

--- eromtima3/mapm.m
+++ eromtima3/mapm.m
@@ -2,19 +2,26 @@ function mapm ()
     Ts=4;
     M=8;
     fc=2.5;
-    bytesrc=floor(rand(1,10002)*2);
+    bytesrc=floor(rand(1,1002)*2);
     symba=[];
     for i = 0:length(bytesrc)/log2(M)-1
         y=i*log2(M)+1;
         ra=bytesrc(y:y+log2(M)-1);
         symba=[symba bin2dec(num2str(ra))];
     endfor
-    gt = @(t) ((t<=0 && t<Ts)*sqrt(2/Ts));
-    sm= @(t,m) (gt(t)*cos(2*pi*t*(fc + m/Ts)));
+    sm= @(t,m) (sqrt(2/Ts)*cos(2*pi*t*(fc + m/Ts)));
     tesr= sm(0.2,5);
     mes= @(m)   (arrayfun(sm, [0:0.1:Ts-0.1], m*ones(1,Ts/0.1)));
     sig=[];
+    sysx=[];
     for i = symba
-        sig= [sig mes(i)];
+        sig= [sig;mes(i)];
     endfor
+    for i = 0:M-1
+        sysx= [sysx mes(i)'];
+    endfor
+    recie=sig*sysx;
+    recie=recie';
+    [tem, fm]=max(recie);
+    fm=fm-1;
 endfunction
