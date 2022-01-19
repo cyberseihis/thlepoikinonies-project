ca78e5aa0de1c91350dbe74aaf4ac5b411254500
cyberseihis <mcraftdm@gmail.com>
2022-01-19 16:02:27 +0200

    some calls erothma 2 anafora

--- erothma2/killerwhale.m
+++ erothma2/killerwhale.m
@@ -4,7 +4,7 @@ SourceA= randn(1,M);
 x= randn(M,1);
 b=1;
 a = [1 0.5 1/3 1/4 1/5 1/6];
-y=filter(b,a,x);
+SourceB=filter(b,a,x)';
 Sqnr=@(x,S) 10*log10(S/x);
 N=[2:4];
 for ni = N
@@ -12,7 +12,13 @@ for ni = N
     [xq, centers, D2]=Vquantizer(SourceA,ni*2);
     disp([Sqnr(D(length(D)),mean(SourceA.^ 2)) , Sqnr(D2,mean(SourceA.^ 2)) , D(length(D)), D2]);
 endfor
-%sxedon akrivos 1
-beh= arrayfun(Sqnr,D,mean(SourceA.^ 2));
+
+for ni = N
+    [xq, centers, D]=Bquantizer(SourceB,ni,min(SourceB),max(SourceB));
+    [xq, centers, D2]=Vquantizer(SourceB,ni*2);
+    disp([Sqnr(D(length(D)),mean(SourceB.^ 2)) , Sqnr(D2,mean(SourceB.^ 2)) , D(length(D)), D2]);
+endfor
+
+beh= arrayfun(Sqnr,D,mean(SourceB.^ 2));
 plot(beh);
 beh;
